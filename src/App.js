288ef17f958021702fb0df9d57f50d726201fc98
efsiqueira <edu.fss14@gmail.com>
2022-08-23 10:38:10 -0300

    removendo linha não utilizada

--- src/App.js
+++ src/App.js
@@ -47,7 +47,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    debugger
     console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
